bb22846ca46f542d2c2ba746baf9c469369358e8
cozyGalvinism <jean@der-capta.in>
2022-10-25 18:11:17 +0200

    Use control flow operator instead of unwrap

--- mcmeta/src/app_config.rs
+++ mcmeta/src/app_config.rs
@@ -19,12 +19,9 @@ impl ServerConfig {
     pub fn from_config() -> Result<Self, MetaMCError> {
         let config = config::Config::builder()
             .add_source(config::Environment::with_prefix("MCMETA"))
-            .set_default("bind_address", "127.0.0.1:8080")
-            .unwrap()
-            .set_default("storage_format", "json")
-            .unwrap()
-            .set_default("meta_directory", "meta")
-            .unwrap()
+            .set_default("bind_address", "127.0.0.1:8080")?
+            .set_default("storage_format", "json")?
+            .set_default("meta_directory", "meta")?
             .build()?;
 
         config.try_deserialize::<'_, Self>().map_err(Into::into)
